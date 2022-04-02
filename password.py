69817fff1c3bee72916b00902e883ce93dc01939
anuananiya16 <102500336+anuananiya16@users.noreply.github.com>
2022-04-02 11:24:40 +0530

    Delete password.py

--- password.py
+++
@@ -1,8 +0,0 @@
-words = ["a", "d", "r", "e"]
-files = open("password.txt", "a")
-for i in words:
-    for j in words:
-        for k in words:
-            for l in words:
-                files.write(f"{i}{j}{k}{l}\n")
-files.close()
